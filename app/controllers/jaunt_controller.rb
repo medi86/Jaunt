20bd83063fd83494d305c362da122039cf0c04cd
Greg Glover <101glover@gmail.com>
2016-05-10 17:29:25 -0500

    make show page

--- app/controllers/jaunt_controller.rb
+++ app/controllers/jaunt_controller.rb
@@ -1,6 +1,5 @@
 class JauntController < ApplicationController
   def home
-
   end
 
   def index
@@ -9,10 +8,13 @@ class JauntController < ApplicationController
 
   def create
     addresses = params[:jaunt][:addresses].map do |pos, loc|
-      Location.new(address: loc[:address], description: loc[:description], position: pos)
+    Location.new(address: loc[:address], description: loc[:description], position: pos)
     end
 
     Jaunt.create(title: params[:jaunt][:jaunt_title], description: params[:jaunt][:jaunt_description], locations: addresses)
-    redirect_to jaunts_path
+  end
+
+  def show
+    @jaunt = Jaunt.find_by_id(params[:id])
   end
 end
