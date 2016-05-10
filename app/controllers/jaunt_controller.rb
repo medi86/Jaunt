a805cab1a2f83adbc8de47fb3256ff07b0fe6e29
Dongmin Lee <medi8600@gmail.com>
2016-05-10 10:48:29 -0500

    headers

--- app/controllers/jaunt_controller.rb
+++ app/controllers/jaunt_controller.rb
@@ -6,6 +6,8 @@ class JauntController < ApplicationController
   def create
     Jaunt.create(title: params[:jaunt][:title], locations: [Location.new(address: params[:jaunt][:startAddress]),
                                                             Location.new(address: params[:jaunt][:endAddress])])
+
+    # @google_api_key = Rails.application.secrets.google_api_key
     redirect_to jaunts_path
   end
 end
