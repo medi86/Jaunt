b39b8c9f230456ae3dc1591cd154161e04b3f4eb
JamesMarkWilton <james.mark.wilton@gmail.com>
2016-05-11 09:42:28 -0500

    Setup Phantomjs and Poltergeist

--- test/integration/jaunt_test.rb
+++ test/integration/jaunt_test.rb
@@ -2,21 +2,46 @@ require 'test_helper'
 
 class JauntTest < ActionDispatch::IntegrationTest
   def test_a_user_can_create_a_new_jaunt
-  start_location = "National Louis University, 122 South Michigan Avenue, Chicago, IL 60603"
-  end_location   = "The Art Institute of Chicago, 111 South Michigan Avenue, Chicago, IL 60603"
+    start_location = "National Louis University, 122 South Michigan Avenue, Chicago, IL 60603"
+    end_location   = "The Art Institute of Chicago, 111 South Michigan Avenue, Chicago, IL 60603"
 
-  page.visit jaunts_path
+    page.visit root_path
+    assert page.has_content?("Create a Jaunt")
+    page.click_link("Create a Jaunt")
 
-  assert page.has_content?("Jaunt Title: ")
-  assert page.has_content?("Start Location: ")
-  assert page.has_content?("End Location: ")
+    require "pry"
+    binding.pry
+    assert page.has_content?("Jaunt Title:")
+    page.fill_in(i"start_location", with: "NLU")
+    assert page.has_content?("Jaunt Description:")
+    assert page.has_content?("End Location: ")
 
-  page.fill_in("start_location", with: "NLU")
-  page.fill_in("end_location", with: "Art Institute of Chicago")
+    page.fill_in("start_location", with: "NLU")
+    page.fill_in("end_location", with: "Art Institute of Chicago")
 
-  page.click_button "Submit Jaunt"
+    page.click_button "Submit Jaunt"
 
-  assert page.has_content? (start_location)
-  assert page.has_content? (end_location)
+    assert page.has_content? (start_location)
+    assert page.has_content? (end_location)
+  end
+
+  def test_a_user_can_find_a_jaunt
+    skip
+    start_location = "National Louis University, 122 South Michigan Avenue, Chicago, IL 60603"
+    end_location   = "The Art Institute of Chicago, 111 South Michigan Avenue, Chicago, IL 60603"
+
+    page.visit jaunts_path
+
+    assert page.has_content?("Jaunt Title: ")
+    assert page.has_content?("Start Location: ")
+    assert page.has_content?("End Location: ")
+
+    page.fill_in("start_location", with: "NLU")
+    page.fill_in("end_location", with: "Art Institute of Chicago")
+
+    page.click_button "Submit Jaunt"
+
+    assert page.has_content? (start_location)
+    assert page.has_content? (end_location)
   end
 end
