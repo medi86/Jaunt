3266b212cc2fe05af64d33d962cf31dfda2dd081
Dongmin Lee <medi8600@gmail.com>
2016-05-12 09:56:35 -0500

    user model, controller

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160509203635) do
+ActiveRecord::Schema.define(version: 20160512143214) do
 
   create_table "jaunts", force: :cascade do |t|
     t.string   "title"
@@ -27,6 +27,13 @@ ActiveRecord::Schema.define(version: 20160509203635) do
     t.integer  "jaunt_id"
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string   "name"
+    t.string   "email"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
 end
