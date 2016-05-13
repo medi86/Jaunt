69e4220abcc13c963522f752cfd7cde8b9f00e63
Greg Glover <101glover@gmail.com>
2016-05-13 09:32:22 -0500

    moved around on the create page layout

--- db/schema.rb
+++ db/schema.rb
@@ -41,6 +41,6 @@ ActiveRecord::Schema.define(version: 20160512152546) do
     t.string   "password_digest"
   end
 
-  add_index "users", ["email"], name: "index_users_on_email", unique: true
+  add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
 
 end
