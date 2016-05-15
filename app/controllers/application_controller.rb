c8375e15201b3f512eef5fd4f1f6508a15594a20
Dongmin Lee <medi8600@gmail.com>
2016-05-14 20:10:23 -0500

    added loginbeforeaction for new jaunt

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,4 +4,11 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   include SessionsHelper
 
+
+  def require_login
+    unless logged_in?
+      flash[:info] = "You must be logged in to access this section"
+      redirect_to signup_path # halts request cycle
+    end
+  end
 end
