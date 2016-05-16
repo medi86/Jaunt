bce7ce1d364152e40f7d664554683b2c6ed52078
JamesMarkWilton <james.mark.wilton@gmail.com>
2016-05-16 09:12:58 -0500

    Testing slowly getting closer to full coverage

--- spec/features/jaunts_spec.rb
+++ spec/features/jaunts_spec.rb
@@ -4,7 +4,6 @@ RSpec.describe('Full Site', {type: :feature}) do
 
   scenario('a user can create a new jaunt', {js: true}) do
     user = User.create!(name: "dongmin", email: "medi8600@gmail.com", password: "asdf1234")
-    nlu = "National Louis University, 122 South Michigan Avenue, Chicago, IL 60603, United States"
 
     page.visit login_path
     page.fill_in("session_email", with: "medi8600@gmail.com")
@@ -14,16 +13,22 @@ RSpec.describe('Full Site', {type: :feature}) do
     page.visit new_jaunt_path
     assert page.has_content?("Jaunt Title")
     page.fill_in("Give this jaunt a name", with: "Test Title")
+
     assert page.has_content?("Jaunt Description")
     page.fill_in("Tell us about what you do on this jaunt...", with: "Test Description")
 
-    page.fill_in("Add a location", with: nlu)
+    page.fill_in("Add a location", with: "national louis")
     page.find_by_id("addLocation").native.send_keys(:arrow_down)
-    #Add in assertion after we find the way to get it to add a location
+    page.find_by_id("addLocation").native.send_keys(:return)
+    page.find_by_id("addLocation").native.send_keys(:arrow_down)
+    page.find_by_id("addLocation").native.send_keys(:return)
+
+    page.click_button("Create Jaunt")
+    assert page.has_content?("Edit this Jaunt")
+    assert page.has_content?("Delete this Jaunt")
   end
 
   scenario('a user who is logged-in can navigate the entire page', {js: true}) do
-    skip
     user = User.create!(name: "dongmin", email: "medi8600@gmail.com", password: "asdf1234")
 
     jaunt = Jaunt.create!(title: 'testJaunt', description: 'testDescription', locations:
@@ -51,13 +56,12 @@ RSpec.describe('Full Site', {type: :feature}) do
     page.click_link("Log-in")
     expect(page.current_path).to eql('/login')
 
-    assert page.has_content?("Create a jaunt")
-    page.click_link("Create a jaunt")
+    assert page.has_content?("Create Jaunts")
+    page.click_link("Create Jaunts")
     expect(page.current_path).to eql('/new')
   end
 
   scenario('a user who is not-logged-in can only navigate the non-user sections of the page', {js: true}) do
-    skip
     jaunt = Jaunt.create!(title: 'testJaunt', description: 'testDescription', locations:
                   [Location.new(address: '123 fake st', name: "fake name", latitude: 41.23, longitude: -87.23)])
 
@@ -78,8 +82,8 @@ RSpec.describe('Full Site', {type: :feature}) do
     page.click_link("Log-in")
     expect(page.current_path).to eql('/login')
 
-    assert page.has_content?("Create a jaunt")
-    page.click_link("Create a jaunt")
+    assert page.has_content?("Create Jaunts")
+    page.click_link("Create Jaunts")
     expect(page.current_path).to eql('/signup')
   end
 
