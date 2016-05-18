86196c5cb1e2bb5b67caf5ce13a8e547cdfd6ea3
Dongmin Lee <medi8600@gmail.com>
2016-05-18 09:45:32 -0500

    facebook oauth working

--- Gemfile
+++ Gemfile
@@ -17,8 +17,10 @@ gem 'bcrypt'
 
 gem "omniauth"
 gem "omniauth-google-oauth2"
+gem "omniauth-twitter"
 gem "devise"
 
+gem 'activerecord-session_store'
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
   gem 'byebug'
--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -1,14 +1,38 @@
 class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
-  def google_oauth2
-    # You need to implement the method below in your model (e.g. app/models/user.rb)
-    @user = User.from_omniauth(request.env["omniauth.auth"])
+  # def google_oauth2
+  #   # You need to implement the method below in your model (e.g. app/models/user.rb)
+  #   @user = User.from_omniauth(request.env["omniauth.auth"])
 
+  #   if @user.persisted?
+  #     flash[:info] = I18n.t "devise.omniauth_callbacks.success", :kind => "Google"
+  #     sign_in_and_redirect @user
+  #   else
+  #     session["devise.google_data"] = request.env["omniauth.auth"]
+  #     redirect_to new_user_registration_url
+  #   end
+  # end
+
+  def facebook
+    generic_callback( 'facebook' )
+  end
+
+  def twitter
+    generic_callback( 'twitter' )
+  end
+
+  def google_oauth2
+    generic_callback( 'google_oauth2' )
+  end
+
+  def generic_callback( provider )
+    @user = User.from_omniauth(request.env["omniauth.auth"])
     if @user.persisted?
-      flash[:info] = I18n.t "devise.omniauth_callbacks.success", :kind => "Google"
+      flash[:info] = I18n.t "devise.omniauth_callbacks.success", :kind => provider.capitalize
       sign_in_and_redirect @user
     else
-      session["devise.google_data"] = request.env["omniauth.auth"]
+      session["devise.#{provider}_data"] = request.env["omniauth.auth"]
       redirect_to new_user_registration_url
     end
   end
 end
+
