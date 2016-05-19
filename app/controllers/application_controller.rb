a3af030d5c55d5d098f485c4255aa36dfad1637d
JamesMarkWilton <james.mark.wilton@gmail.com>
2016-05-19 15:27:00 -0500

    Add better Errors and fix the Update issue

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,21 +1,16 @@
 class ApplicationController < ActionController::Base
-  # Prevent CSRF attacks by raising an exception.
-  # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
   include SessionsHelper
   include JauntHelper
 
-
   def require_login
     unless logged_in?
-      flash[:info] = "You must be logged in to access this section"
-      redirect_to signup_path # halts request cycle
+      flash[:danger] = "You must be logged in to access this section"
+      redirect_to signup_path
     end
   end
 
   def sign_in_and_redirect(resource_or_scope, *args)
-    # options  = args.extract_options!
-    # scope    = Devise::Mapping.find_scope!(resource_or_scope)
     resource = args.last || resource_or_scope
     log_in(resource)
     redirect_to after_sign_in_path_for(resource)
